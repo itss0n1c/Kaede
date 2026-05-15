b3ba89a81757aea50907894eb357bd3d102bc1ad
s0n1c <me@s0n1c.ca>
2026-05-15 18:22:34 -0400

    feat: update flake configuration to include bunix as an input

--- flake.nix
+++ flake.nix
@@ -2,45 +2,23 @@
   description = "kaede dev environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.bunix = {
+    url = "github:S0n1cHoldings/bunix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    nixpkgs,
+    bunix,
+    ...
+  }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
-
-    bun_1_3_14 = pkgs.stdenv.mkDerivation {
-      pname = "bun";
-      version = "1.3.14";
-
-      src = pkgs.fetchzip {
-        url = "https://github.com/oven-sh/bun/releases/download/bun-v1.3.14/bun-linux-x64.zip";
-        hash = "sha256-YyGDD7f0JlmiO2G3LY80p/oMUpWXcoC7x7LW/gU/LmU=";
-        stripRoot = false;
-      };
-
-      nativeBuildInputs = with pkgs; [
-        autoPatchelfHook
-      ];
-
-      buildInputs = with pkgs; [
-        stdenv.cc.cc.lib
-        zlib
-      ];
-
-      installPhase = ''
-        runHook preInstall
-
-        mkdir -p $out/bin
-        cp bun-linux-x64/bun $out/bin/bun
-        chmod +x $out/bin/bun
-
-        runHook postInstall
-      '';
-    };
   in {
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         zsh
-        bun_1_3_14
+        bunix.packages.${system}.default
         biome
         nodejs_22
       ];
